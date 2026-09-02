c8fa55c5b3f96c7b369f172a7cc5daec4f573642
Yrodalisa Kornet <s1222119@student.windesheim.nl>
2026-09-02 22:12:14 +0200

    feat: klasse Recept geïmplementeerd met private attributen en string-representatie

---
+++ recept.py
@@ -0,0 +1,9 @@
+class Recept:
+    def __init__(self, naam, omschrijving):
+        self.__naam = naam
+        self.__omschrijving = omschrijving
+        self.__ingredient_list = []
+        self.__stappen = []
+
+    # etc.. etc..
+    # Succes!
