f2dd330fc1648dc7f6a36a281890d25f00837aee
Yrodalisa Kornet <s1222119@student.windesheim.nl>
2026-09-03 20:01:51 +0200

    data: recept Quesadillas met gehakt voor 1 persoon toegevoegd

--- main.py
+++ main.py
@@ -6,27 +6,50 @@ def main():
     recepten = []
 
     
-    recept1 = Recept("Pasta Pesto", "Een romige pasta met kip en pesto voor 1 persoon.")
+    recept1 = Recept("Tagliatelle pesto", "Een romige pasta met kip en pesto voor 1 persoon.")
 
     recept1.voeg_ingredient_toe(Ingredient("kip", 100, "gram"))
-    recept1.voeg_ingredient_toe(Ingredient("pesto", 1, "eetlepels"))
-    recept1.voeg_ingredient_toe(Ingredient("ui", 1, "kwart"))
-    recept1.voeg_ingredient_toe(Ingredient("pasta", 75, "gram"))
-    recept1.voeg_ingredient_toe(Ingredient("kookroom", 62.5, "milliliter"))
-    recept1.voeg_ingredient_toe(Ingredient("cherry tomaatjes", 62.5, "gram"))
-    recept1.voeg_ingredient_toe(Ingredient("peper en zout", 0.25, "snuf"))
-    recept1.voeg_ingredient_toe(Ingredient("olijfolie", 1, "klein scheutje"))
+    recept1.voeg_ingredient_toe(Ingredient("pesto", 100, "gram"))
+    recept1.voeg_ingredient_toe(Ingredient("uitje", 1, ""))
+    recept1.voeg_ingredient_toe(Ingredient("tagliatelle", 150, "gram"))
+    recept1.voeg_ingredient_toe(Ingredient("kookroom", 100, "milliliter"))
+    recept1.voeg_ingredient_toe(Ingredient("cherry tomaatjes", 250, "gram"))
+    recept1.voeg_ingredient_toe(Ingredient("peper en zout", 1, "snuf"))
+    recept1.voeg_ingredient_toe(Ingredient("olijfolie", 1, "scheutje"))
     recept1.voeg_ingredient_toe(Ingredient("geraspte kaas", 1, "handje"))
+    recept1.voeg_ingredient_toe(Ingredient("knoflook", 1, "teentje"))
 
-    recept1.voeg_stap_toe(Stap("Snipper het uitje en fruit even aan in een scheutje olijfolie. Voeg de blokjes kip toe en bak ongeveer 5 minuten. Kook ondertussen de pasta gaar."))
+    recept1.voeg_stap_toe(Stap("Snipper het uitje en knoflook en fruit even aan in een scheut olijfolie. Voeg de blokjes kip toe en bak ongeveer 5 minuten. Kook ondertussen de tagliatelle gaar."))
     recept1.voeg_stap_toe(Stap("Voeg de (zelfgemaakte) pesto en room toe aan de kip en roer goed door. Proef nog even of er nog peper of zout bij moet."))
-    recept1.voeg_stap_toe(Stap("Laat de pestosaus een paar minuutjes zachtjes pruttelen. Voeg dan de gekookte pasta toe en schep er doorheen."))
-    recept1.voeg_stap_toe(Stap("Snijd de cherry tomaatjes door de helft en verwarm deze nog een minuutje mee. Serveer de pasta pesto op een bord, eventueel met wat geraspte kaas."))
+    recept1.voeg_stap_toe(Stap("Laat de pestosaus een paar minuutjes zachtjes pruttelen. Voeg dan de gekookte tagliatelle toe en schep er doorheen."))
+    recept1.voeg_stap_toe(Stap("Snijd de cherry tomaatjes door de helft en verwarm deze nog een minuutje mee. Serveer de tagliatelle pesto op een bord, eventueel met wat geraspte kaas."))
 
     recepten.append(recept1)
 
-    recept2 = Recept("Gehakt quiche met paprika", "Een heerlijke quiche met gehakt en paprika.")
+    recept2 = Recept("Quesadillas met gehakt", "Heerlijke quesadillas met gehakt voor 1 persoon.")
 
+    recept2.voeg_ingredient_toe(Ingredient("gehakt", 175, "gram"))
+    recept2.voeg_ingredient_toe(Ingredient("tortilla's", 2, "stuks"))
+    recept2.voeg_ingredient_toe(Ingredient("paprika", 1, ""))
+    recept2.voeg_ingredient_toe(Ingredient("uitje", 1, "klein"))
+    recept2.voeg_ingredient_toe(Ingredient("maïs", 140, "gram uit blik"))
+    recept2.voeg_ingredient_toe(Ingredient("zure room", 125, "milliliter"))
+    recept2.voeg_ingredient_toe(Ingredient("geraspte kaas", 1, "handje"))
+    recept2.voeg_ingredient_toe(Ingredient("peper en zout", 1, "snuf"))
+    recept2.voeg_ingredient_toe(Ingredient("komijn", 1, "snuf"))
+    recept2.voeg_ingredient_toe(Ingredient("paprikapoeder", 1, "snuf"))
+    recept2.voeg_ingredient_toe(Ingredient("tomatenpuree", 1, "eetlepel"))
+    recept2.voeg_ingredient_toe(Ingredient("guacamole", 2, "eetlepels"))
+
+    recept2.voeg_stap_toe(Stap("Doe de tomatenpuree met de komijn, paprikapoeder en gehakt in een pan en bak een paar minuutjes. Voeg eventueel een klein scheutje water toe om het mengsel iets smeuïger te maken."))
+    recept2.voeg_stap_toe(Stap("Snijd de puntpaprika in kleine blokjes, laat de maïs uitlekken en snipper de rode ui."))
+    recept2.voeg_stap_toe(Stap("Neem een wrap en bestrijk deze met zure room. Verdeel een deel van het gehaktmengsel er over."))
+    recept2.voeg_stap_toe(Stap("Verdeel dan ook een deel van de blokjes paprika, maïs, rode ui en geraspte kaas er over."))
+    recept2.voeg_stap_toe(Stap("Leg de tweede wrap er bovenop en druk deze een beetje aan. Bak de quesadilla in een droge koekenpan op middelhoog vuur totdat de onderkant goudbruin is. Draai hem voorzichtig om en bak ook de andere kant goudbruin."))
+    recept2.voeg_stap_toe(Stap("Dek de wrap af met een andere wrap en bak de quesadilla in een contactgrill of in een droge koekenpan licht krokant."))
+    recept2.voeg_stap_toe(Stap("Snijd de quesadilla in vieren en serveer met guacamole en zure room."))
+    
+    recepten.append(recept2)
     
 
 if __name__ == "__main__":
