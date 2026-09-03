b9b99876b5135f4c472bb19e3f6d44b7aac54c79
Yrodalisa Kornet <s1222119@student.windesheim.nl>
2026-09-03 19:45:28 +0200

    data: recept Pasta Pesto voor 1 persoon toegevoegd aan main.py

--- main.py
+++ main.py
@@ -5,19 +5,29 @@ from stap import Stap
 def main():
     recepten = []
 
-    recept1 = Recept("Kip Kerrie", "Kip kerrie zonder pakjes en zakjes")
-    #recept1.voeg_ingredient_toe(Ingredient("kip", 500, "gram"))
-    #recept1.voeg_ingredient_toe(Ingredient("sperziebonen", 400, "gram"))
+    
+    recept1 = Recept("Pasta Pesto", "Een romige pasta met kip en pesto voor 1 persoon.")
 
-    #recept1.voeg_stap_toe(Stap("Kook de rijst en zet een pan water met een snuf zout op het vuur voor de sperziebonen."))
-    #recept1.voeg_stap_toe(Stap("Snijd de kip in kleine blokjes, snipper het uitje, snijd de knoflook fijn en snijd de kontjes van de sperziebonen (was ze ook even)."))
+    recept1.voeg_ingredient_toe(Ingredient("kip", 100, "gram"))
+    recept1.voeg_ingredient_toe(Ingredient("pesto", 1, "eetlepels"))
+    recept1.voeg_ingredient_toe(Ingredient("ui", 1, "kwart"))
+    recept1.voeg_ingredient_toe(Ingredient("pasta", 75, "gram"))
+    recept1.voeg_ingredient_toe(Ingredient("kookroom", 62.5, "milliliter"))
+    recept1.voeg_ingredient_toe(Ingredient("cherry tomaatjes", 62.5, "gram"))
+    recept1.voeg_ingredient_toe(Ingredient("peper en zout", 0.25, "snuf"))
+    recept1.voeg_ingredient_toe(Ingredient("olijfolie", 1, "klein scheutje"))
+    recept1.voeg_ingredient_toe(Ingredient("geraspte kaas", 1, "handje"))
 
-    #recepten.append(recept1)
+    recept1.voeg_stap_toe(Stap("Snipper het uitje en fruit even aan in een scheutje olijfolie. Voeg de blokjes kip toe en bak ongeveer 5 minuten. Kook ondertussen de pasta gaar."))
+    recept1.voeg_stap_toe(Stap("Voeg de (zelfgemaakte) pesto en room toe aan de kip en roer goed door. Proef nog even of er nog peper of zout bij moet."))
+    recept1.voeg_stap_toe(Stap("Laat de pestosaus een paar minuutjes zachtjes pruttelen. Voeg dan de gekookte pasta toe en schep er doorheen."))
+    recept1.voeg_stap_toe(Stap("Snijd de cherry tomaatjes door de helft en verwarm deze nog een minuutje mee. Serveer de pasta pesto op een bord, eventueel met wat geraspte kaas."))
+
+    recepten.append(recept1)
 
     recept2 = Recept("Gehakt quiche met paprika", "Een heerlijke quiche met gehakt en paprika.")
 
-    # enz.. enz..
-    # Veel succes!
+    
 
 if __name__ == "__main__":
     main()
